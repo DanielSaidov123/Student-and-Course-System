eb552955ffc6f54cd66edfa4fd76510785ff5e1f
DanielSaidov123 <ddny3622@gmail.com>
2025-12-24 12:22:17 +0200

    finish

--- controllers/SearchAndFilter.js
+++ controllers/SearchAndFilter.js
@@ -11,10 +11,10 @@ export const SearchStudentsByName = async (req, res) => {
     const name = req.query.name;
     const studentlist = await readStudents();
     const student = studentlist.filter((s) => s.name.includes(name));
-    if (student.length===0) {
+    if (student.length === 0) {
       return res.status(404).send({ msg: "student is not defind." });
     }
-    res.status(200).send({student});
+    res.status(200).send({ student });
   } catch (err) {
     res.status(500).send(err);
   }
@@ -25,11 +25,55 @@ export const SearchCursesByName = async (req, res) => {
     const name = req.query.name;
     const curseslist = await readCourses();
     const curses = curseslist.filter((c) => c.instructor.includes(name));
-    if (curses.length===0) {
+    if (curses.length === 0) {
       return res.status(404).send({ msg: "curses is not defind." });
     }
-    res.status(200).send({curses});
+    res.status(200).send({ curses });
   } catch (err) {
     res.status(500).send(err);
   }
+};
+const validCursesByName = async (req, res) => {
+  try {
+    const instructor = req.query.instructor;
+    const curseslist = await readCourses();
+    const curses = curseslist.filter((c) => c.instructor.includes(instructor));
+    if (curses.length === 0) {
+      return res.status(404).send({ msg: "curses is not defind." });
+    }
+    res.status(200).send({ curses });
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
+
+const validByCreditRange = async (req, res) => {
+  try {
+    const minCredits = parseInt(req.query.minCredits);
+    const maxCredits = parseInt(req.query.maxCredits);
+    if (!Number.isInteger(maxCredits) || !Number.isInteger(minCredits)) {
+      return res
+        .status(404)
+        .send({ msg: "minCredits or maxCredits is not number" });
+    }
+    const curseslist = await readCourses();
+    const curses = curseslist.filter(
+      (c) => c.credits > minCredits && c.credits < maxCredits
+    );
+    if (curses.length === 0) {
+      return res.status(404).send({ msg: "curses is not defind." });
+    }
+    res.status(200).send({ curses });
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
+
+export const FilterCoursesByType = async (req, res) => {
+  const instructor = req.query.instructor;
+  const minCredits = parseInt(req.query.minCredits);
+  const maxCredits = parseInt(req.query.maxCredits);
+  console.log(minCredits);
+  if (minCredits && maxCredits) validByCreditRange(req, res);
+  else if (instructor) validCursesByName(req, res);
 };
--- routes/Courses.js
+++ routes/Courses.js
@@ -1,14 +1,14 @@
 import express from "express";
 import { CreateCurses, DelCurse, GetCurse, GetCurses, updatingCurse } from "../controllers/Courses.js";
 import { GetStudentOfCurses } from "../controllers/Enrollments.js";
-import { SearchCursesByName } from "../controllers/SearchAndFilter.js";
+import {FilterCoursesByType } from "../controllers/SearchAndFilter.js";
 
 const router = express.Router();
 
 
 
 router.route('/').get(GetCurses).post(CreateCurses)
-router.route('/search').get(SearchCursesByName )
+router.route('/search').get(FilterCoursesByType)
 router.route('/:id').get(GetCurse).put(updatingCurse).delete(DelCurse)
 router.route('/:courseId/students').get(GetStudentOfCurses)
 
