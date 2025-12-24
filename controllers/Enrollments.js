0636c95e703b95a972af581849429c75fb55c359
DanielSaidov123 <ddny3622@gmail.com>
2025-12-24 10:49:56 +0200

    add GetCursesOfStudent

--- controllers/Enrollments.js
+++ controllers/Enrollments.js
@@ -51,16 +51,43 @@ export const RemoveStudentFromCourse = async (req, res) => {
     }
     for (let i = 0; i < student.enrolledCourses.length; i++) {
       if (student.enrolledCourses[i] === curses.id) {
-        student.enrolledCourses.splice(i,1)
+        student.enrolledCourses.splice(i, 1);
         writeStudents(studentlist);
         res.status(200).send(student);
       }
     }
-     return res
-        .status(404)
-        .send({ msg: "He is not registered for this course." });
+    return res
+      .status(404)
+      .send({ msg: "He is not registered for this course." });
   } catch (err) {
     res.status(500).send({ err });
   }
 };
 
+export const GetCursesOfStudent = async (req, res) => {
+  try {
+    const studentId = req.params.studentId;
+    const curseslist = await readCourses();
+    const studentlist = await readStudents();
+    const student = studentlist.find((s) => s.id == studentId);
+    if (!student) {
+      return res.status(404).send({ msg: "student is not defind." });
+    }
+    const arr = [];
+    for (let i = 0; i < student.enrolledCourses.length; i++) {
+      for (let j = 0; j < curseslist.length; j++) {
+        if (student.enrolledCourses[i] === curseslist[j].id) {
+          arr.push(curseslist[j]);
+        }
+      }
+    }
+    if (arr.length === 0) {
+      return res
+        .status(404)
+        .send({ msg: "He is not registered for  courses." });
+    }
+    res.status(200).send({ msg: arr});
+  } catch (err) {
+    res.status(500).send({ err });
+  }
+};
--- routes/Students.js
+++ routes/Students.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { CreateStudent, DelStudent, GetStudens ,GetStudent, updatingSstudent} from "../controllers/Students.js";
-import { AddingCourseToStudent, RemoveStudentFromCourse } from "../controllers/Enrollments.js";
+import { AddingCourseToStudent, GetCursesOfStudent, RemoveStudentFromCourse } from "../controllers/Enrollments.js";
 
 const router = express.Router();
 
@@ -8,8 +8,9 @@ const router = express.Router();
 
 router.route('/').get(GetStudens).post(CreateStudent)
 router.route('/:id').get(GetStudent).put(updatingSstudent).delete(DelStudent)
+router.route('/:studentId/courses').get(GetCursesOfStudent)
 router.route('/:studentId/enroll/:courseId').post(AddingCourseToStudent).delete(RemoveStudentFromCourse)
-router.route('/:studentId/courses').post()
+
 
  
 
